89c1fe86f4ef98b1cb4b885b779409eb2d162271
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-09-25 09:14:19 -0300

    Atualização de código - 25/09/2025

--- src/main/java/com/senai/Conta_Bancaria/application/service/ContaService.java
+++ src/main/java/com/senai/Conta_Bancaria/application/service/ContaService.java
@@ -1,5 +1,32 @@
 package com.senai.Conta_Bancaria.application.service;
 
-public class ContaService {
+import com.senai.Conta_Bancaria.application.dto.ContaResumoDTO;
+import com.senai.Conta_Bancaria.domain.entity.Conta;
+import com.senai.Conta_Bancaria.domain.repository.ContaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+@Transactional
+
+public class ContaService {
+    private final ContaRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<ContaResumoDTO> listarTodasContas() {
+        return repository.findAllByAtivaTrue().stream()
+                .filter(Conta::isAtiva)
+                .map(ContaResumoDTO::fromEntity).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ContaResumoDTO buscarContaPorNumero(String numero) {
+        return ContaResumoDTO.fromEntity(
+                repository.findByNumeroAndAtivaTrue(numero)
+                        .orElseThrow(() -> new RuntimeException("Conta não encontrada"))
+        );
+    }
 }
