74ad69b5e35ba4e49fff1f4640f3d2c5523cde24
Gabrieli da Silva Marcelino <gabih.12161@gmail.com>
2025-10-21 09:55:53 -0300

    Correção e atualização de código - 21/10/2025

--- src/main/java/infrastructure/config/MethodSecurityConfig.java
+++ src/main/java/infrastructure/config/MethodSecurityConfig.java
@@ -1,6 +1,7 @@
 package infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
 @EnableMethodSecurity
